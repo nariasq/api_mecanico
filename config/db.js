87602d6012feddacb4a1f675eddb8305c657cfae
Nicolas Arias <ariasnicolas200506@gmail.com>
2026-02-16 15:27:52 -0500

    API lista para deploy

--- config/db.js
+++ config/db.js
@@ -2,19 +2,16 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || process.env.DB_PORT
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error("❌ Error conectando MySQL:", err);
-  } else {
-    console.log("✅ MySQL conectado");
-  }
+  if (err) console.error(err);
+  else console.log("✅ MySQL conectado");
 });
 
 module.exports = db;
